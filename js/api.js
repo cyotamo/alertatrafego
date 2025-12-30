cbfd6303a89547974fa5c2e6dd0346fc5b27bd57
cyotamo <cyotamo@yahoo.com.br>
2025-12-30 15:27:54 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "COLOCAR_AQUI_URL_DO_WEB_APP";
+const API_URL = "https://script.google.com/macros/s/AKfycbxz8YIIEbYVflpApwZhBfhVrlc85sC1CyqxMGuK3qwLykH6VNkcwStNxiKVQ7PcXr6h/exec";
 
 async function apiGet(action) {
   const res = await fetch(`${API_URL}?action=${action}`);
